4741a1c2a92b10066c9b5799f067b8b6dd47db23
Swamy3232 <swamiaws85@gmail.com>
2025-12-05 20:07:17 +0530

    dd

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://pyxujweejwvyphjmxaag.supabase.co";
-// Use SERVICE ROLE key here (never expose this to frontend)
-const supabaseServiceKey = "sb_publishable_m-lUbx-eEnhKYmXt0FkLQw_T-8llcsQ";
+// Use your SECRET key here, never expose it to frontend
+const supabaseSecretKey = "sb_secret_Jqm1U"; 
 
-export const supabase = createClient(supabaseUrl, supabaseServiceKey);
+export const supabase = createClient(supabaseUrl, supabaseSecretKey);
