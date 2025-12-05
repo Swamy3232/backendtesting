1019db78a9ea7ba68452c59634f57f0e91f95d18
Swamy3232 <swamiaws85@gmail.com>
2025-12-05 20:05:53 +0530

    d

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://pyxujweejwvyphjmxaag.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5eHVqd2Vland2eXBoam14YWFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5MjM3ODYsImV4cCI6MjA4MDQ5OTc4Nn0.JoawKcnSLQO6tMYZWJZEEPNsqgggQeaeYJS8ilFA0q8";
+// Use SERVICE ROLE key here (never expose this to frontend)
+const supabaseServiceKey = "sb_publishable_m-lUbx-eEnhKYmXt0FkLQw_T-8llcsQ";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseServiceKey);
